f3a87cbb50122f7318166c9a32d4cc4b21b44565
mlaflam <mlaflam@users.noreply.github.com>
2024-05-05 01:15:17 -0400

    Hourly Weather Edits

--- WeatherApp/src/components/HourlyForecast.jsx
+++ WeatherApp/src/components/HourlyForecast.jsx
@@ -9,17 +9,23 @@ const HourlyForecast = ({ data }) => {
     const temp = kelvinToFahrenheit(hour.main.temp);
     const imgId = hour.weather[0].icon;
 
-    return <Hour key={hour.dt} time={time} temp={temp} imgId={imgId} />;
+    return <Hour key={hour.dt} time={time} temp={temp + "º"} imgId={imgId} />;
   });
 
   function kelvinToFahrenheit(kelvin) {
     return ((kelvin - 273.15) * 9/5 + 32).toFixed(0);
   }
 
-  function convertUnixToTime(unixTimestamp) {
-    const date = new Date(unixTimestamp * 1000);
-    return date.toLocaleTimeString([], { hour: '2-digit'});
+function convertUnixToTime(unixTimestamp) {
+  const date = new Date(unixTimestamp * 1000);
+  let hour = date.getHours() % 12; // Get hour in 12-hour format
+  if (hour === 0) {
+    hour = 12; // Convert 0 to 12 for 12-hour format
   }
+  const ampm = date.getHours() >= 12 ? 'PM' : 'AM'; // Determine if it's AM or PM
+  return hour.toString() + ampm;
+}
+
 
   return (
     <div className="hourly-forecast-container">
